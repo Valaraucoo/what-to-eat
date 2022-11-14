3f09427edc2372b479af76c71ab7a62a2cf95921
Kamil Woźniak <41345824+Valaraucoo@users.noreply.github.com>
2022-11-15 00:17:32 +0100

    Github-Actions: Modify check-version actions (#6)
    🐱🔨 [github-actions] added check if version was update on pull request to master branch

--- .github/actions/check-versions/app/main.py
+++ .github/actions/check-versions/app/main.py
@@ -2,12 +2,14 @@ import re
 from pathlib import Path
 from typing import Final
 
+import httpx
 from pydantic import BaseSettings, SecretStr
 
 
 class Settings(BaseSettings):
     github_repository: str
     github_ref: str
+    github_base_ref: str
     input_github_token: SecretStr
 
 
@@ -30,13 +32,49 @@ def get_what_to_eat_version() -> str:
     content = what_to_eat_version_file.read_text()
     match = re.search(version_regex, content)
     if match is None:
-        raise ValueError("Could not find version in what_to_eat/__init__.py")
+        raise ValueError(f"[{settings.github_ref}] Could not find version in what_to_eat/__init__.py")
     return match.group("version")
+
+
+def get_master_version() -> str:
+    response = httpx.get(
+        f"https://raw.githubusercontent.com/{settings.github_repository}/master/pyproject.toml"
+    )
+    response.raise_for_status()
+    content = response.text
+    match = re.search(version_regex, content)
+    if match is None:
+        raise ValueError("[master] Could not find version in pyproject.toml")
+    return match.group("version")
+
+
+def should_compare_versions() -> bool:
+    return settings.github_base_ref == "master"
+
+
+def is_updated(old: str, new: str) -> bool:
+    new = new.split(".")
+    old = old.split(".")
+    for i in range(3):
+        if int(new[i]) > int(old[i]):
+            return True
+        elif int(new[i]) < int(old[i]):
+            return False
+    return False
 
 
 if __name__ == "__main__":
     pyproject_toml_version = get_pyproject_toml_version()
     what_to_eat_version = get_what_to_eat_version()
+
+    if should_compare_versions():
+        master_version = get_master_version()
+        if not is_updated(pyproject_toml_version, master_version):
+            raise ValueError(
+                f"Version in pyproject.toml ({pyproject_toml_version}) "
+                f"must be greater than version in master ({master_version})"
+            )
+
     if pyproject_toml_version != what_to_eat_version:
         raise ValueError(
             f"Version mismatch between pyproject.toml and what_to_eat/__init__.py: "
